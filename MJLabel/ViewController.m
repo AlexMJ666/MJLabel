9c551d69e42570733e54150ae4d2b683ad3f9ada
马家俊 <921711730@qq.com>
2017-03-16 16:40:50 +0800

    自定义label

---
+++ MJLabel/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MJLabel
+//
+//  Created by 马家俊 on 17/3/16.
+//  Copyright © 2017年 MJJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
